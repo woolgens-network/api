f8b1101e5ae74f1aa261125c3f0a483e7267de40
ReaperMaga <reapermaga@gmail.com>
2021-11-09 22:14:50 +0100

    Added WoolgensConstants

---
+++ src/main/java/net/woolgens/api/WoolgensConstants.java
@@ -0,0 +1,12 @@
+package net.woolgens.api;
+
+/**
+ * Copyright (c) Maga, All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Maga
+ **/
+public final class WoolgensConstants {
+
+    public static final String PREFIX = "§f§lWool§c§lGens §8╠ §7";
+}
