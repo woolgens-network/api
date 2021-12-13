6084b2b67379734bc03c9decd121e1a817c11bba
ReaperMaga <reapermaga@gmail.com>
2021-12-13 10:34:14 +0100

    Added onReward() method to QuestRewardListener

--- src/main/java/net/woolgens/api/quest/model/QuestRewardListener.java
+++ src/main/java/net/woolgens/api/quest/model/QuestRewardListener.java
@@ -1,5 +1,8 @@
 package net.woolgens.api.quest.model;
 
+import net.woolgens.api.user.User;
+import org.bukkit.entity.Player;
+
 /**
  * Copyright (c) Maga, All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
@@ -7,4 +10,6 @@ package net.woolgens.api.quest.model;
  * Written by Maga
  **/
 public interface QuestRewardListener {
+
+    void onReward(User user, Player player);
 }
