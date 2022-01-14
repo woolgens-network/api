93d92cf2767669a4cef30a2f6c49e3f05234266c
ReaperMaga <reapermaga@gmail.com>
2022-01-14 18:17:57 +0100

    Added extensions map to user and season data

--- src/main/java/net/woolgens/api/user/data/SeasonData.java
+++ src/main/java/net/woolgens/api/user/data/SeasonData.java
@@ -22,4 +22,5 @@ public class SeasonData {
     private SeasonQuestData quests;
     private Map<String, Long> stats;
     private Map<String, Integer> crates;
+    private Map<String, Object> extensions;
 }
